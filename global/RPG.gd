e09df97d36910d04e4aa5c414089707e4ca2861a
YeOldeDM <nines@email.com>
2017-06-09 01:40:25 -0600

    cleanup for merge

--- global/RPG.gd
+++ global/RPG.gd
@@ -1,12 +1,11 @@
 extends Node
 
+const GREETING = "Hello RPG!"
+
 onready var _db = preload( "res://things/Database.tscn" ).instance()
 
 
 func make_thing( path ):
 	return _db.spawn( path )
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+
